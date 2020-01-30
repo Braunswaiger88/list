f6010829b3c662b5edcf251e486a6b4e9be09f64
Braunswaiger88 <58085067+Braunswaiger88@users.noreply.github.com>
2020-01-30 23:42:35 +0200

    array lab

---
+++ array.php
@@ -0,0 +1,9 @@
+<?php
+    $students[0]="Сидоров Василий Петрович";
+    $students[1]="Василий Петр Василевич";
+    $students[2]="Петров Сидр Сидорович";
+
+
+    echo $students[0] ."<br>";
+    echo $students[1] ."<br>";
+    echo $students[2] ."<br>";
